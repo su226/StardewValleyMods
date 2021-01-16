59706073d9c1ee6c2ba74639538b25a8d44b5706
su226 <1368981939@qq.com>
2021-01-16 18:14:39 +0800

    [GreatEnchanter] Remove wrong comment.

--- GreatEnchanter/GreatEnchanter.cs
+++ GreatEnchanter/GreatEnchanter.cs
@@ -21,15 +21,12 @@ namespace Su226.GreatEnchanter {
         return;
       }
       if (!Game1.player.CanMove) {
-        Monitor.Log("Can't save: Can't move");
         return;
       }
       if (Game1.activeClickableMenu != null) {
-        Monitor.Log("Can't save: Menu open");
         return;
       }
       if (Game1.currentMinigame != null) {
-        Monitor.Log("Can't save: Minigame open");
         return;
       }
       Game1.playSound("bigSelect");
