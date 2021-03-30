504b698a24a462710888d4a15176faef52b2819d
su226 <1368981939@qq.com>
2021-03-30 14:55:02 +0800

    [FieldRing] Initial version.

---
+++ FieldRing/Config.cs
@@ -0,0 +1,12 @@
+﻿namespace Su226.FieldRing {
+  class Config {
+    public int Index = 538603905;
+    public int Range = 128;
+    public int Damage = 10;
+    public bool PercentageDamage = true;
+    public bool IgnoreResilience = true;
+    public bool IgnoreMissChance = true;
+    public bool IgnoreCooldown = false;
+    public float Knockback = 8;
+  }
+}
---
+++ FieldRing/FieldRingMod.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+
+namespace Su226.FieldRing {
+  class M {
+    public static IModHelper Helper;
+    public static IMonitor Monitor;
+    public static Config Config;
+  }
+  class FieldRingMod : Mod {
+    private Texture2D EffectTexture;
+
+    public override void Entry(IModHelper helper) {
+      M.Helper = Helper;
+      M.Monitor = Monitor;
+      M.Config = helper.ReadConfig<Config>();
+      helper.Events.Display.RenderedWorld += OnRenderedWorld;
+      FieldRingItem.ItemTexture = helper.Content.Load<Texture2D>("assets/field_ring.png");
+      EffectTexture = helper.Content.Load<Texture2D>("assets/field_effect.png");
+      helper.ConsoleCommands.Add("fieldring_add", "Add a field ring to player's inventory.", FieldRingAdd);
+    }
+    
+    private void FieldRingAdd(string name, string[] arguments) {
+      Game1.player.addItemToInventory(new FieldRingItem());
+    }
+
+    private void OnRenderedWorld(object o, RenderedWorldEventArgs e) {
+      foreach (Farmer f in Game1.currentLocation.farmers) {
+        if (f.isWearingRing(M.Config.Index)) {
+          Point center = f.GetBoundingBox().Center;
+          e.SpriteBatch.Draw(EffectTexture, new Rectangle(center.X - M.Config.Range - Game1.viewport.X, center.Y - M.Config.Range - Game1.viewport.Y, M.Config.Range * 2, M.Config.Range * 2), Color.White);
+        }
+      }
+    }
+  }
+}
